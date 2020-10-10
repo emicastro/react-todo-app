7bc42d4f14865d2d8e897c6830c0a52bbc20a2b4
Emilio Castro <emilio.castro@mercadolibre.com>
2020-10-10 12:22:57 -0300

    Updates

---
+++ precache-manifest.dc7a14ec9244c8bc091cbc383662f445.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bebfad574c145caf4f0c536f5d380de",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "ca04bcfae024b2454919",
+    "url": "/react-todo-app/static/css/main.42dfab62.chunk.css"
+  },
+  {
+    "revision": "a73896d069c6b450b0b9",
+    "url": "/react-todo-app/static/js/2.04fc1425.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-app/static/js/2.04fc1425.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca04bcfae024b2454919",
+    "url": "/react-todo-app/static/js/main.42338c64.chunk.js"
+  },
+  {
+    "revision": "7f607c3e693deaba8e3a",
+    "url": "/react-todo-app/static/js/runtime-main.93dbfc51.js"
+  }
+]);
